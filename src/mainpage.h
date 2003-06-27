71ca1229cfa91f0a1a59e7e5c225e429639a81c0
jrb <>
2003-06-27 17:24:22 +0000

    Improve documentation of diagnostic programs

--- src/mainpage.h
+++ src/mainpage.h
@@ -8,16 +8,37 @@ program which invokes it. Other applications linking to the library may also
 use the classes.  The entire package may someday become a subpackage 
 of detModel.</p>
 <p>
-To date two functions are provided:</p>
+To date three functions are provided:</p>
 <ul>
- <li>Overlap checking.  See the class Overlaps.  It examines all sibling
+ <li>Overlap checking, executable test.exe.
+  See the class Overlaps and main program 
+  overlapsMain.cxx.  It examines all sibling
 positioned volumes (children of the same composition or stack) to see
 that they don't overlap.  It also checks that all children of a composition
 are contained in its envelope.</li>
- <li>Summary materials statistics.  See the class SolidStats.  It outputs
+ <li>Documentation of constants, executable constsDoc.exe.  
+See the main program constsDocMain.cxx,
+  which invokes detModel utilities to produce html output.</li>
+ <li>Summary materials statistics, executable summary.exe.  
+See the class SolidStats and main
+program solidStatsMain.cxx.  It outputs
 numbers of volumes made of each material, total cubic volume of each
 material, and so forth.</li>
 </ul>
+<p>These programs are invoked as follows (optional arguments are in
+[square brackets])</p>
+<pre>
+   <b>test.exe</b> aPath/myGeoInput.xml [anotherPath/output.txt [verbose [dump] ] ]
+</pre>
+<p>where the last two arguments are boolean.  By default both are false,
+but if present (any string) they are true.</p>
+<pre>
+   <b>constsDoc.exe</b> aPath/myGeoInput.xml anotherPath/myGeoOutput.html
+
+   <b>summary.exe</b> aPath/myGeoInput.xml  anotherPath/mySummary.html [topVolume  [choice-mode] ]
+</pre>
+<p>See sample output from the last two programs at 
+<a href="http://www.slac.stanford.edu/exp/glast/gorund/software/geomtry/data/RELEASED/">http://www.slac.stanford.edu/exp/glast/gorund/software/geomtry/data/RELEASED/</a></p>
 <hr>
   \section requirements cmt/requirements
   \include requirements
