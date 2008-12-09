d1a0700dd5ddbaf5fda5b0b1c69edeae36c2730f
ecephas <>
2008-12-09 00:08:44 +0000

    Check in detCheckLib.py for detCheck v1r7 (branch-tag, GlastReleasev15r49).

--- detCheckLib.py
+++ detCheckLib.py
@@ -1,7 +1,9 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/detCheck/detCheckLib.py,v 1.1 2008/08/15 21:42:38 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['detCheck'])
-
+    env.Tool('addLibrary', library = env['xercesLibs'])
+    env.Tool('detModelLib')
+    env.Tool('identsLib')
 def exists(env):
     return 1;
